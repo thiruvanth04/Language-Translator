c3f68cbb45ed724ee071e2f822a3c1ef9bffb8d0
thiruvanth04 <166989329+thiruvanth04@users.noreply.github.com>
2024-07-22 19:07:04 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Adjust the paths according to your project structure
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0066b2',
+      }
+    },
+  },
+  plugins: [],
+};
